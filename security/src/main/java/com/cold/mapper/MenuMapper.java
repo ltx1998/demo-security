d9b15059213dd73fd5fa74b7b6d38f041d74cb50
ltx1998 <18970230295@163.com>
2022-06-18 17:44:45 +0800

    update

--- security/src/main/java/com/cold/mapper/MenuMapper.java
+++ security/src/main/java/com/cold/mapper/MenuMapper.java
@@ -10,7 +10,9 @@ import java.util.List;
  * @create: 2022-06-12 10:39
  */
 public interface MenuMapper extends BaseMapper<Menu> {
+
     /**
+     * 根据用户id查询权限
      * @param userId
      * @return
      */
